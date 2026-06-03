3e46c61690655d2866c3473ed21e491596f53537
Aysenur Svs <aysenursivasligil966@gmail.com>
2026-06-03 10:51:18 +0300

    Change bnb_4bit_compute_dtype to torch.float16 for improved performance

--- src/train.py
+++ src/train.py
@@ -71,7 +71,7 @@ def main():
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
+        bnb_4bit_compute_dtype=torch.float16
     )
 
     model = AutoModelForCausalLM.from_pretrained(
